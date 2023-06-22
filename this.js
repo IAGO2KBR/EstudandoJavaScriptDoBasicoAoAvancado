5fea2fba639a524f1e248f9c9686d459d8be3d33
IAGOADS <m>
2023-06-22 20:31:56 -0300

    adicionando o arquivo this.js

---
+++ this.js
@@ -0,0 +1,30 @@
+/* Dentro de uma função podemos declarar uma variavél
+   com o mesmo nome de seus parâmetros e receber o valores
+   utilzando o operador This.
+*/
+
+function aluno (nome, nota){
+    this.nome = nome;
+    this.nota = nota;
+
+    this.exibirNomeNotaComFunction = function() {
+        setTimeout(function(){
+            console.log(this.nome)
+            console.log(this.nota)
+        },3000)
+    }
+
+    this.exibirNomeNotaComArrowFunction = function(){
+        setTimeout(()=>{
+            console.log(this.nome)
+            console.log(this.nota)
+        },3000)
+    }
+}
+const aluno1 = new aluno("Iago",100)
+aluno1.exibirNomeNotaComFunction()
+aluno1.exibirNomeNotaComArrowFunction()
+
+/* O método exibirNomeNotaComFunction ele utiliza o contexto do setTimeout, logo no console.log estamos
+criando uma nova instância, por isso na saída ocorre undefined, Já exibirNomeNotaComArrowFunction, a Arrow function utiliza 
+o contexto do elemento Pai que é a função aluno.  */
